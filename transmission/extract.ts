16297b05f856bbe51d13b8eecdf064b37f30af39
lpenaud <l.penaud@zaclys.net>
2025-07-26 04:23:43 +0200

    FIX(transmission): Update dataset

--- transmission/extract.ts
+++ transmission/extract.ts
@@ -22,6 +22,7 @@ export function fromReadable(
     .pipeThrough(
       new CsvParseStream({
         separator: ";",
+        skipFirstRow: true,
         columns: [
           "date",
           "downloadedBytes",
